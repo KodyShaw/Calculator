740566e889d54f9d46fe4c93cd197e110526d4aa
KodyShaw <kodyshaw16@gmail.com>
2018-10-09 19:24:29 -0700

    possible solution 10/9

--- calculator.js
+++ calculator.js
@@ -54,22 +54,41 @@ switch (command) {
         break;
     case 'divide':
 
-        for (i = 1; i < numArray.length; i++) {
-            if (numArray[i] === 0) {
-                consol.log("You canot divide by zero!");
-            }
-            else {
-                var previousTotal = numArray[0]; //Inital number in array       
-
-                for (i = 1; i < numArray.length; i++) {
-                    previousTotal = previousTotal / numArray[i]; // for each number in array divide to the previous number
-                }
-            }
-
-            result = previousTotal // Pushes end total to result
-        }
-
+    if (numArray.length < 2) {
+        console.log("no numbers in array")
         break;
+    }
+    // previousTotal starts with 10
+    var previousTotal = numArray[1]; 
+
+    // start from the second number which is 2
+    for (i = 2; i < numArray.length; i++) {
+        if (numArray[i] === 0) {
+            console.log("You canot divide by zero!");
+        }
+        else {
+            previousTotal += previousTotal / numArray[i]; // for each number in array divide to the previous number
+        }
+    }
+    result = previousTotal;
+    // result will be (10/2)/5 = 1
+    break;
+        // for (i = 1; i < numArray.length; i++) {
+        //     if (numArray[i] === 0) { // If any number besides the first is 0 spit out this
+        //         consol.log("You canot divide by zero!");
+        //     }
+        //     else {
+        //         var previousTotal = numArray[0]; //Inital number in array       
+
+        //         for (i = 1; i < numArray.length; i++) {
+        //             previousTotal = previousTotal / numArray[i]; // for each number in array divide to the previous number
+        //         }
+        //     }
+
+        //     result = previousTotal // Pushes end total to result
+        // }
+
+        // break;
     case 'remainder':
 
         for (i = 1; i < numArray.length; i++) {
